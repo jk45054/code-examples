3580cfe538ee11f15ca70efe5cc62e7a53907ad5
jk45054 <69588950+jk45054@users.noreply.github.com>
2023-10-17 12:40:32 +0200

    Update decode_instruction.py

--- IDA/scripts/decode_instruction.py
+++ IDA/scripts/decode_instruction.py
@@ -1,8 +1,14 @@
-# Decode Instruction at ScreenEA
 import ida_ua
 import ida_kernwin
 ea = ida_kernwin.get_screen_ea()
 insn = ida_ua.insn_t()
 len_inst = ida_ua.decode_insn(insn, ea)
 if len_inst > 0:
-  print(insn.get_canon_mnem())
+  print(f"get_canon_mnem: {insn.get_canon_mnem()}")
+  print(f"get_canon_feature: {insn.get_canon_feature()}")
+  print(f"itype: {insn.itype}")
+  # Opx is of type op_t
+  print(f"op1: {insn.Op1}")
+  print(f"op2: {insn.Op2}")
+  print(f"op3: {insn.Op3}")
+  
